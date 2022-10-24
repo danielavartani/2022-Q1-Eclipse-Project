b3c9dd1fe8060ccb3bdb8549d243eef5ef57550a
1853775 <1853775@01-19811.pusd.dom>
2022-10-24 09:02:20 -0700

    added Classes

---
+++ Classes/src/Runner.java
@@ -0,0 +1,31 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 30;
+		double y =3.14;
+		boolean notTrue = false;
+		
+		//classes in java are Datatypes
+		Phone myPhone; //the phone class is being used as the type
+		Planet mars;
+		Dog fido; 
+		
+		myPhone = new Phone ();
+		Phone myPhone2 = new Phone();
+		
+		//Planet object?
+		Planet pluto = new Plantet ();
+		
+		//Dog object?
+		Dog jak = new Dog ();
+		
+		
+		
+		System.out.println(Dog.feetToMeters(50));		
+		
+	}
+
+}
